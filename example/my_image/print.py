96619fe94ac2bb7cb4511e3f5011855fd098b23b
QinjieLin-NU <qinielin2018@u.northwestern.edu>
2023-05-18 20:09:49 +0000

    update example

--- example/my_image/print.py
+++ example/my_image/print.py
@@ -1,12 +1,1 @@
 print("helloworld")
-
-import torch
-
-# Check the number of available CUDA devices
-device_count = torch.cuda.device_count()
-print("Number of CUDA devices:", device_count)
-
-# Get the name of each CUDA device
-for i in range(device_count):
-    device_name = torch.cuda.get_device_name(i)
-    print(f"CUDA device {i}: {device_name}")
